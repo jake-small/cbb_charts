34d217cdf223cac1836e09e0ec23dd5940bb79c2
jake-small <jakesmall@protonmail.com>
2022-12-04 19:02:36 -0800

    create chart

--- cbb_stacked_area_chart.py
+++ cbb_stacked_area_chart.py
@@ -1,4 +1,5 @@
 from cbb_playbyplay import *
+from stacked_area_chart import make_stackd_area_chart
 
 
 def get_bottom_of_minute_scores(scores_by_minute):
@@ -11,7 +12,7 @@ def get_bottom_of_minute_scores(scores_by_minute):
             seen_minutes.add(score['minute'])
     final_score_each_minute.reverse()
 
-    bottom_of_minute_scores = [{'minute': 0, 'score': 0, 'player': ''}]
+    bottom_of_minute_scores = []  # [{'minute': 0, 'score': 0, 'player': ''}]
     minute = 1
     for index, score in enumerate(final_score_each_minute):
         if (score['minute'] != minute):
@@ -27,12 +28,16 @@ def get_bottom_of_minute_scores(scores_by_minute):
     return bottom_of_minute_scores
 
 
-def format_scores_for_stacked_area_chart(bottom_of_minute_scores):
-    # e.g. formatted_scores = {'home': [1, 4, 6, 8, 9,...], 'away': [1, 4, 6, 8, 9,...]}
-    formatted_scores = []
+def get_just_scores_per_minute(bottom_of_minute_scores):
+    just_scores = []
     for score in bottom_of_minute_scores:
-        formatted_scores.append(score['score'])
-    return formatted_scores
+        just_scores.append(score['score'])
+    return just_scores
+
+
+def format_scores_for_stacked_area_chart(home_scores, away_scores):
+    # e.g. formatted_scores = {'home': [1, 4, 6, 8, 9,...], 'away': [1, 4, 6, 8, 9,...]}
+    return {'home': home_scores, 'away': away_scores}
 
 
 pbp_data = get_espn_data("playbyplay", 401479681)
@@ -42,15 +47,15 @@ made_shots = get_made_shots(playbyplay)
 home_scores_by_minutes = get_scores_by_minute(made_shots, 'home')
 home_bottom_of_minute_scores = get_bottom_of_minute_scores(
     home_scores_by_minutes)
-home_scores = format_scores_for_stacked_area_chart(
+home_scores = get_just_scores_per_minute(
     home_bottom_of_minute_scores)
 
 away_scores_by_minutes = get_scores_by_minute(made_shots, 'away')
 away_bottom_of_minute_scores = get_bottom_of_minute_scores(
     away_scores_by_minutes)
-away_scores = format_scores_for_stacked_area_chart(
+away_scores = get_just_scores_per_minute(
     away_bottom_of_minute_scores)
 
-
-print(home_scores)
-print(away_scores)
+stacked_area_data = format_scores_for_stacked_area_chart(
+    home_scores, away_scores)
+make_stackd_area_chart(stacked_area_data)
